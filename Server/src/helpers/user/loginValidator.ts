d259b183a29e9f520856c03d18845e2b25a395c3
kithekadk <dankinyi99@gmail.com>
2022-10-29 17:10:15 +0300

    removed server from this reposiry

--- Server/src/helpers/user/loginValidator.ts
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const LoginValidator= Joi.object({
-    email: Joi.string().required().email().message('Email must be a valid email'),
-    password: Joi.string().required().min(8).message('Too long password')
-})
--- Server/src/helpers/user/userValidator.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi'
-
-export const userValidator = Joi.object({
-    fullName: Joi.string().required(),
-    userName: Joi.string().required(),
-    email: Joi.string().required().email(),
-    phoneNumber: Joi.number().required(),
-    password: Joi.string().required(),
-})
